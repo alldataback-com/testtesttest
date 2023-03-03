c968869056485e10cb983dcf1f6d57273d5bc86a
flashdata.io <sunnydreamspace@gmail.com>
2023-03-03 15:41:11 +0800

    Update mo.js

--- dddd/mo.js
+++ dddd/mo.js
@@ -1,7 +1,7 @@
 let rHead = '</style>';
 let newStyle = '@media all and (max-width: 860px)
 .mobile-warning {
-    display: none;
+    display: none !important;
 }</style>';
 let body = $response.body
     .replace(rHead, newStyle);
