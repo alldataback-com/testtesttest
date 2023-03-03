f9b6331d842d458e27ddf4e4b8cefe59a2d41e68
flashdata.io <sunnydreamspace@gmail.com>
2023-03-03 16:22:28 +0800

    Update mo.js

--- dddd/mo.js
+++ dddd/mo.js
@@ -1,9 +1,5 @@
 let rHead = '</style>';
-let newStyle = '@media all and (max-width:860px){
-    .mobile-warning{
-        display:none !important
-    }
-}</style>';
+let newStyle = '.mobile-warning{ display:none !important}</style>';
 let body = $response.body
     .replace(rHead, newStyle);
 $done({ body });
